1aac44be186ab18faff9aa609181df434f2559c3
Vaishnavi U P <vaishnaviup@LAPTOP-GR6LNRCL.localdomain>
2026-01-11 16:46:40 +0000

    Day 3: PointerArithmetic - Array Traversal

--- 07_Pointers/pointerarithmetic.c
+++ 07_Pointers/pointerarithmetic.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
+	int i=0;
 	int arr[4];
 	printf("Enter 4 elements: ");
-	for(int i=0; i<4; i++)
+	for(i=0; i<4; i++)
 	{
 		scanf("%d",&arr[i]);
 	}
 	int *p=arr; //address of first element of an array
-	printf("%p\n",p);
 	printf("Forward Array Traversal: \n");
-	while(p<(int *)(&arr+1)) 
+	for(i=0; i<4; i++) 
 	//&arr means address of whole array
 	//&arr+1 means address of one step ahead of entire array
 	//(int *)(&arr+1) type cast converts it to pointer to int 
@@ -22,7 +22,7 @@ int main()
 	printf("Backward Array Traversal: \n");
 	p--;	
 //After Forward Array Traversal p moves to one step ahead of entire array address, so decrementing to last element
-	while(p>=arr)
+	for(i=0; i<4; i++)
 	{
 		printf("%p : %d\n",(void *)p,*p);
 		p--;
